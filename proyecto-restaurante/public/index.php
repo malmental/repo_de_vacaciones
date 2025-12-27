d79f86670ae942f104b3631629de68963fb58857
malmental <orlandorojasoyarzun@gmail.com>
2025-12-27 21:30:31 +0100

    se esta conectado index con el procesamiemto de datos

---
+++ proyecto-restaurante/public/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESERVA DE MESAS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+    <h1>RESERVA DE MESAS</h1>
+
+    <form method="post" action="informacion_reserva.php">
+        
+        <div class="campo">
+            <label for="cliente">Nombre de cliente:</label>
+            <input type="text" name="cliente" id="cliente">
+        </div>
+
+        <div class="campo">
+            <label for="telefono">Numero de teléfono: </label>
+            <input type="text" name="numero_de_telefono" id="numero_de_telefono" placeholder="YYYY/MM/DD">
+        </div>
+
+        <div class="campo">
+            <label for="email">Corrreo electrónico: </label>
+            <input type="text" name="email" id="email" placeholder="hola@email.net">
+        </div>
+
+        <div class="campo">
+            <label for="fecha_de_reserva">Fecha para su reserva: </label>
+            <input type="text" name="fecha_reserva" id="fecha_reserva">
+        </div>
+
+        <div class="campo">
+            <label for="hora">Hora de reserva: </label>
+            <input type="text" name="hora" id="hora" placeholder="HH:MM">
+        </div>
+
+        <div class="campo">
+            <label for="numero_de_personas">Numero de personas: </label>
+            <input type="text" name="num_personas" id="num_personas">
+        </div>
+
+        <div class="campo">
+            <label for="numero_de_mesa">Numero de mesa 1-9: </label>
+            <input type="text" name="numero_mesa" id="numero_mesa">
+        </div>
+
+        <div class="campo">
+            <label for="ubicacion">'Terraza', 'Interior', 'Ventana': </label>
+            <input type="text" name="ubicacion" id="ubicacion">
+        </div>
+
+        <div class="campo">
+            <label for="observaciones">Observaciones: </label>
+            <textarea name="descripcion" id="descripcion" rows="3"></textarea>
+        </div>
+
+        <button>Enviar</button>
+     </form>
+     </div>
+</body>
+</html>
